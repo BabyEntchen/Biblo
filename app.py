474f5cc0ae18e5f02ee44a3de9ebe4cafde98391
BabyEntchen <69644451+BabyEntchen@users.noreply.github.com>
2024-04-12 15:14:33 +0200

    updates

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from utils.books import Book
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', book_list=[Book.isbn_get("9783837641097"), Book.isbn_get("9783551551672")])
 
 
 if __name__ == '__main__':
